d288c8f75e355dc479996d40232a4678380d8dd3
Hirushi7 <hirushind7@gmail.com>
2026-08-27 20:53:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Paintings from "./pages/Paintings";
 import Experience1 from "./pages/Experience1";
